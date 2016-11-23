6f9701bcf0cd5f2f32932a306557dd16bd59ea76
rahul830ui <rahul830ui@gmail.com>
2016-11-23 03:50:11 -0800

    Final Changes

--- app/components/TodoSearch.jsx
+++ app/components/TodoSearch.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 
 var TodoSearch = React.createClass({
     handleSearch: function () {
-        var showCompleted = this.refs.showCompleaed.checked;
+        var showCompleted = this.refs.showCompleted.checked;
         var searchText = this.refs.searchText.value;
         this.props.onSearch(showCompleted, searchText);
 
@@ -16,7 +16,7 @@ var TodoSearch = React.createClass({
                 </div>
                 <div>
                     <label>
-                        <input type="checkbox" ref='showCompleaed' onChange={this.handleSearch}/>
+                        <input type="checkbox" ref='showCompleted' onChange={this.handleSearch}/>
                         Show Compleated Todos
                     </label>
 
--- app/tests/components/AddTodo.test.jsx
+++ app/tests/components/AddTodo.test.jsx
@@ -14,10 +14,10 @@ describe('AddTodo', () => {
   it('should call onAddTodo prop with valid data', () => {
     var todoText = 'Check mail';
     var spy = expect.createSpy();
-    var addTodo = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy}/>);
+    var addTodo = TestUtils.renderIntoDocument(<AddTodo newTodo={spy}/>);
     var $el = $(ReactDOM.findDOMNode(addTodo));
 
-    addTodo.refs.todoText.value = todoText;
+    addTodo.refs.todoItem.value = todoText;
     TestUtils.Simulate.submit($el.find('form')[0]);
 
     expect(spy).toHaveBeenCalledWith(todoText);
@@ -26,10 +26,10 @@ describe('AddTodo', () => {
   it('should not call onAddTodo prop when invalid input', () => {
     var todoText = '';
     var spy = expect.createSpy();
-    var addTodo = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy}/>);
+    var addTodo = TestUtils.renderIntoDocument(<AddTodo newTodo={spy}/>);
     var $el = $(ReactDOM.findDOMNode(addTodo));
 
-    addTodo.refs.todoText.value = todoText;
+    addTodo.refs.todoItem.value = todoText;
     TestUtils.Simulate.submit($el.find('form')[0]);
 
     expect(spy).toNotHaveBeenCalled();
