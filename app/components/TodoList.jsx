c019e76d500635fbee9ee5a031fc9fdc18274da9
rahul830ui <rahul830ui@gmail.com>
2016-11-29 02:37:33 -0800

    refactor with todoSearch

--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -1,10 +1,11 @@
 var React = require('react');
 var {connect} = require('react-redux');
 import Todo from 'Todo';
+var TodoAPI = require('TodoAPI');
 
 export var TodoList = React.createClass({
     render: function () {
-        var {todos} = this.props;
+        var {todos, showCompleted, searchText} = this.props;
         var renderTodods = function () {
             if (todos.length === 0) {
                 return (
@@ -12,7 +13,7 @@ export var TodoList = React.createClass({
 
                 );
             }
-            return todos.map(function (todo){
+            return TodoAPI.filterTodos(todos, showCompleted, searchText).map(function (todo){
                 return (
                     <Todo key={todo.id}{...todo}/>
                 )
@@ -30,9 +31,6 @@ export var TodoList = React.createClass({
 
 export default connect(
     (state) => {
-        return {
-            todos: state.todos
-        };
-
+        return state;
     }
 )(TodoList);
